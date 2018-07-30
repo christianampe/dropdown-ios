165ef82104817203ee80e12507a20000469617df
christianampe <christianampe.work@gmail.com>
2018-07-30 12:30:09 -0700

    added FlatField dependency

--- Example/Pods/Target Support Files/Pods-FlatDropdown_Example/Pods-FlatDropdown_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-FlatDropdown_Example/Pods-FlatDropdown_Example-frameworks.sh
@@ -144,9 +144,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/FlatDropdown/FlatDropdown.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FlatField/FlatField.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/FlatDropdown/FlatDropdown.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FlatField/FlatField.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
