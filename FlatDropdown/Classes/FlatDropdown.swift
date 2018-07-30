e498b97780ef131db6b3038b49b9c1b78c9554bc
christianampe <christianampe.work@gmail.com>
2018-07-30 12:45:44 -0700

    unstable state until FlatLabel pod is updated

--- FlatDropdown/Classes/FlatDropdown.swift
+++ FlatDropdown/Classes/FlatDropdown.swift
@@ -5,4 +5,77 @@
 //  Created by Ampe on 7/30/18.
 //
 
-import Foundation
+import FlatField
+
+// MARK: - Class Declaration
+@IBDesignable
+open class FlatDropdown {
+    
+    // MARK: Views
+    weak var flatField: FlatField!
+    weak var tableView: UITableView!
+    
+    // MARK: IBInspectables
+    @IBInspectable
+    open var text: String = FlatFieldConfig.default.text {
+        didSet {
+            flatField.textField.text = text
+        }
+    }
+    
+    @IBInspectable
+    open var placeholderText: String = FlatFieldConfig.default.placeholderText {
+        didSet {
+            flatField.textField.placeholder = placeholderText
+        }
+    }
+    
+    @IBInspectable
+    open var cursorColor: UIColor = FlatFieldConfig.default.cursorColor {
+        didSet {
+            flatField.textField.tintColor = cursorColor
+        }
+    }
+    
+    @IBInspectable
+    open var textColor: UIColor = FlatFieldConfig.default.textColor {
+        didSet {
+            flatField.textField.textColor = textColor
+        }
+    }
+    @IBInspectable
+    open var placeholderColor: UIColor = FlatFieldConfig.default.placeholderColor {
+        didSet {
+            flatField.textField.attributedPlaceholder = NSAttributedString(string: placeholderText, attributes:[NSAttributedStringKey.foregroundColor: placeholderColor])
+        }
+    }
+    
+    @IBInspectable
+    open var underlineColor: UIColor = FlatFieldConfig.default.underlineColor {
+        didSet {
+            flatField.underline.backgroundColor = underlineColor
+        }
+    }
+    
+    @IBInspectable
+    open var underlineThickness: CGFloat = FlatFieldConfig.default.underlineThickness {
+        didSet {
+            flatField.underlineHeightConstraint.constant = underlineThickness
+        }
+    }
+    
+    @IBInspectable
+    open var textAlignment: Int = FlatFieldConfig.default.textAlignment.rawValue {
+        didSet {
+            guard let alignment = NSTextAlignment(rawValue: textAlignment) else {
+                assert(false, "use a valid alignment mapping integer (0-4)")
+                return
+            }
+            
+            textField.textAlignment = alignment
+        }
+    }
+    
+    @IBInspectable
+    open var thicknessChange: CGFloat = FlatFieldConfig.default.thicknessChange
+}
---
+++ FlatDropdownConfig.swift
@@ -0,0 +1,13 @@
+//
+//  FlatDropdownConfig.swift
+//  FlatDropdown
+//
+//  Created by Ampe on 7/30/18.
+//
+
+import Foundation
+
+public struct FlatDropdownConfig {
+    let numberOfResults: Int
+    let cellReuseIdenfier: String
+}
